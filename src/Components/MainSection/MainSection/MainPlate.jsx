b5d94ded6e4e29cf0119762f0202c5a2275e9d86
superhe11 <yankevichyaroslav@gmail.com>
2024-10-07 16:51:51 +0300

    fix: fixed prettier and eslint

--- src/Components/MainSection/MainSection/MainPlate.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const MainPlate = ({ children }) => {
-    return <main>{children}</main>;
-};
-
-export default MainPlate;
--- src/Components/Redux/logger.js
+++ src/Components/Redux/logger.js
@@ -1,6 +1,9 @@
-export const loggerMiddleware = ({ getState }) => (next) => (action) => {
-    console.log('Dispatching action:', action);
-    const result = next(action);
-    console.log('Next state:', getState()); 
-    return result; 
-};
+export const loggerMiddleware =
+    ({ getState }) =>
+    (next) =>
+    (action) => {
+        console.log('Dispatching action:', action);
+        const result = next(action);
+        console.log('Next state:', getState());
+        return result;
+    };
